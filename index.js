2f11ea380f5ab0443f8353038694e31851fc7ef6
dcardenas-robert <drcpcardenas@gmail.com>
2024-10-05 19:55:25 -0600

    ampiando las posoibilidaddes y ejemplos de router

--- index.js
+++ index.js
@@ -9,9 +9,32 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('hola soy una nueva ruta');
 });
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
-    name: 'Product 1',
-    price: 1000,
+    id: id,
+    name: 'Product 2',
+    price: 2000,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productsId', (req, res) => {
+  const { categoryId, productsId } = req.params;
+  res.json({
+    categoryId,
+    productsId,
   });
 });
 
