9dadc7ef37a233e4d6c6f12e74bf16e8021a5c4e
dcardenas-robert <drcpcardenas@gmail.com>
2024-10-05 21:15:27 -0600

    como enviar codigos de status

--- routes/products.router.js
+++ routes/products.router.js
@@ -23,7 +23,10 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
+  if (id == '999') {
+    res.status(404).send('Producto no encontrado');
+  }
+  res.status(201).json({
     id: id,
     name: 'Product 2',
     price: 2000,
@@ -32,7 +35,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'Creacion producto',
     data: body,
   });
