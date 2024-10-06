a8c3d92180cd9a54fd34dedc4d6ec6e03fe0f342
dcardenas-robert <drcpcardenas@gmail.com>
2024-10-05 19:44:27 -0600

    Probando routing

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Esta sera el inicio d euna gran historia');
 });
+app.get('/nueva-ruta', (req, res) => {
+  res.send('hola soy una nueva ruta');
+});
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000,
+  });
+});
 
 app.listen(port, () => {
   console.log('Mi port' + port);
