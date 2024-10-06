9a14cada954012d2c1462995397fefc57cd67dac
dcardenas-robert <drcpcardenas@gmail.com>
2024-10-05 21:00:58 -0600

    Haciendo post en productos utilizando insomia para tal accion

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Esta sera el inicio d euna gran historia');
@@ -10,52 +13,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('hola soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('Haciendo filtro');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id: id,
-    name: 'Product 2',
-    price: 2000,
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parametros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productsId', (req, res) => {
-  const { categoryId, productsId } = req.params;
-  res.json({
-    categoryId,
-    productsId,
-  });
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Mi port' + port);
---
+++ routes/products.router.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
+    });
+  }
+  res.json(products);
+});
+
+router.get('/filter', (req, res) => {
+  res.send('Haciendo filtro');
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id: id,
+    name: 'Product 2',
+    price: 2000,
+  });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'Creacion producto',
+    data: body,
+  });
+});
+
+module.exports = router;
