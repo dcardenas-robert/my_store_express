75123d49166031239febfe4b0b9a50c47c04e381
dcardenas-robert <drcpcardenas@gmail.com>
2024-10-05 20:23:37 -0600

    utilizando faker para crear datos falsos en un objeto

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 const app = express();
 const port = 3000;
 
@@ -8,17 +9,23 @@ app.get('/', (req, res) => {
 app.get('/nueva-ruta', (req, res) => {
   res.send('hola soy una nueva ruta');
 });
+
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000,
-    },
-    {
-      name: 'Product 2',
-      price: 2000,
-    },
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
+    });
+  }
+  res.json(products);
+});
+
+app.get('/products/filter', (req, res) => {
+  res.send('Haciendo filtro');
 });
 
 app.get('/products/:id', (req, res) => {
@@ -28,6 +35,18 @@ app.get('/products/:id', (req, res) => {
     name: 'Product 2',
     price: 2000,
   });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros');
+  }
 });
 
 app.get('/categories/:categoryId/products/:productsId', (req, res) => {
